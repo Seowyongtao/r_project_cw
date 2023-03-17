8bd13f27d7edc8b2a578502f489b97a14be133fe
Seow Yong Tao <seowyongtao22@gmail.com>
2023-03-17 10:26:57 +0800

    Done question 7

---
+++ question7.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+tag_by_year <- read.csv('/Users/seowyongtao/Desktop/R_learn/fiv_cw/QueryResults.csv')
+
+BiTools <- c('powerbi', 'tableau-api')
+
+year_total <- tag_by_year %>% 
+  group_by(Year) %>%
+  summarize(Year_total = sum(Number))
+
+tag_by_year <- tag_by_year %>%
+                  filter(TagName %in% BiTools)
+
+tag_by_year_total <- tag_by_year %>%
+                      left_join(year_total, by = "Year")
+
+tag_percentage <- tag_by_year_total %>%
+                      mutate(Percentage = Number/Year_total*100)
+
+
+fig <- ggplot(tag_percentage, aes(x=Year, y=Percentage, color=TagName)) + geom_line() +
+        ggtitle("Growing Trend of PowerBi and Tableau") 
+
+ggplotly(fig)
